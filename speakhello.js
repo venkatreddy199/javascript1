80af49b4fd57ba993f79c5c4b9ea8cb0ef2a808c
venkatreddy199 <104723730+venkatreddy199@users.noreply.github.com>
2022-05-28 11:02:05 +0530

    Add files via upload

---
+++ speakhello.js
@@ -0,0 +1,8 @@
+(function (window){
+  let helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name){
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
